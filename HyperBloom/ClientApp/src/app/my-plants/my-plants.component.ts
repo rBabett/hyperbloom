6fa734c472974cff9eab375fbe213b966da47c79
rBabett <richterbabett03@gmail.com>
2023-10-10 10:57:10 +0200

    make it possible to delete plants

--- HyperBloom/ClientApp/src/app/my-plants/my-plants.component.ts
+++ HyperBloom/ClientApp/src/app/my-plants/my-plants.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {getBaseUrl} from "../../main";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-my-plants',
@@ -7,15 +9,37 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MyPlantsComponent {
   public plants: Plant[] = [];
-
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  private http: HttpClient;
+  private router: Router;
+  constructor(http: HttpClient,
+              @Inject('BASE_URL') baseUrl: string,
+              private Router: Router) {
     http.get<Plant[]>(baseUrl + 'api/plants').subscribe(result => {
       this.plants = result;
     }, error => console.error(error));
+    this.http = http;
+    this.router = Router;
+  }
+
+  public DeletePlant(id: number) {
+    this.http.delete(getBaseUrl() + 'api/plants/' + id).subscribe({
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    })
+    this.router.navigateByUrl('/', { skipLocationChange: true })
+      .then(() => {
+        this.router.navigate(['my-plants']);
+      });
+  }
+
+  public UpdatePlant(id: number) {
+    this.router.navigate(['my-plants/' + id]);
   }
 }
 
 interface Plant {
+  id: number;
   name: string;
   abbreviation: string;
   lightNeeds: string;
