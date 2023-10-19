1928ca1b8d707502a3cbcd421d61af284f129992
rBabett <richterbabett03@gmail.com>
2023-10-19 16:59:19 +0200

    add garden-details component

---
+++ HyperBloom/ClientApp/src/app/garden-details/garden-details.component.ts
@@ -0,0 +1,77 @@
+import {Component, Inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormBuilder} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {GardenService} from "../garden.service";
+import {Router} from "@angular/router";
+import {Cell, Garden, MyGardensComponent} from "../my-gardens/my-gardens.component";
+import {Plant} from "../my-plants/my-plants.component";
+import {PlantService} from "../plant.service";
+
+
+@Component({
+  selector: 'app-garden-details',
+  templateUrl: './garden-details.component.html',
+  styleUrls: ['./garden-details.component.css']
+})
+export class GardenDetailsComponent {
+  public garden: Garden | undefined;
+  public id: number;
+  public name: string | undefined;
+  public columns: number | undefined;
+  public rows: number | undefined;
+  public cells: Cell[] | undefined;
+  public plants: Plant[] | undefined;
+  public selectedPlant: Plant | null | undefined;
+
+  private http: HttpClient;
+  private baseUrl: string;
+  private router: Router;
+
+  newCellsForm = this.formBuilder.group({
+    cells: [],
+  });
+
+  constructor(http: HttpClient,
+              @Inject('BASE_URL') baseUrl: string,
+              private formBuilder: FormBuilder,
+              private Router: Router,
+              private route: ActivatedRoute,
+              public gardenService: GardenService,
+              private plantService: PlantService) {
+
+    let id = Number(this.route.snapshot.paramMap.get('id'));
+    this.id = id;
+
+    plantService.getPlants().subscribe(res => {
+      this.plants = res;
+    })
+
+    this.gardenService.getGardenById(id).subscribe(result => {
+      this.id = result.gardenId
+      this.garden = result;
+      this.name = result.name;
+      this.columns = result.columns;
+      this.rows = result.rows;
+      this.cells = result.cells;
+    }, error => console.error(error));
+
+    this.http = http;
+    this.baseUrl = baseUrl;
+    this.router = Router;
+  }
+
+  onSelect(plant: any): void {
+    this.selectedPlant = plant;
+  }
+
+  sowSeed(cell: any, plant: any): void {
+    cell.plant = plant;
+  }
+
+  protected readonly MyGardensComponent = MyGardensComponent;
+
+  onSubmit(id: number) {
+    this.gardenService.updateGardenCells(id, this.cells);
+  }
+}
