db678920cd5ed6ed754c03ad06f3d553f4282f87
rBabett <richterbabett03@gmail.com>
2023-11-06 14:59:25 +0100

    add more info to seed class

--- HyperBloom/Models/Entities/Seed.cs
+++ HyperBloom/Models/Entities/Seed.cs
@@ -14,9 +14,29 @@ public class Seed
     [Required]
     public string Name { get; set; }
     public string Color { get; set; }
-    public Seed(string name, string color)
+    public string LightNeeds { get; set; }
+    public string WaterNeeds { get; set; }
+    public string SoilNeeds { get; set; }
+    public double ExpectedHarvestAmount { get; set; }
+    public double ActualHarvestAmount { get; set; }
+    public int LowerTemp { get; set; }
+    public int HigherTemp { get; set; }
+    public string TempUnit { get; set; }
+    public string HarvestUnit { get; set; }
+
+    public Seed(string name, string lightNeeds, string waterNeeds, string soilNeeds, string color,
+        int expectedHarvestAmount, string harvestUnit, int lowerTemp, int higherTemp, string tempUnit)
     {
         Name = name;
+        LightNeeds = lightNeeds;
+        WaterNeeds = waterNeeds;
+        SoilNeeds = soilNeeds;
         Color = color;
+        ExpectedHarvestAmount = expectedHarvestAmount;
+        ActualHarvestAmount = 0;
+        HarvestUnit = harvestUnit;
+        LowerTemp = lowerTemp;
+        HigherTemp = higherTemp;
+        TempUnit = tempUnit;
     }
 }
