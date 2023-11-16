702bf2909d8144095c187d9dab5b18471b829b80
rBabett <richterbabett03@gmail.com>
2023-11-16 16:30:56 +0100

    remove token from cookie on logout

--- HyperBloom/ClientApp/src/app/services/user.service.ts
+++ HyperBloom/ClientApp/src/app/services/user.service.ts
@@ -1,8 +1,8 @@
 import {Inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Router} from "@angular/router";
-import {Observable} from "rxjs";
 import {getBaseUrl} from "../../main";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -15,41 +15,51 @@ export class UserService {
     this.Http = http;
   }
 
-  loginUser(formData: {
-    Username: string;
-    Password: string;
-  }){
+  loginUser(formData: { Username: string | null | undefined; Password: string | null | undefined }) :string{
     this.Http.post<LoginModel>(getBaseUrl() + 'api/user/login', formData).subscribe(
       (data) => {
         this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
           this.router.navigate(['/']);
+          window.location.reload();
         });
       },
       (error) => {
         console.error('There was an error!', error);
+        return error.message.split("//");
       }
     )
+    return "";
   }
 
   logoutUser() {
     this.Http.post(getBaseUrl() + 'api/user/logout', {}).subscribe(
       (data) => {
-        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-          this.router.navigate(['/']);
+        document.cookie.split(";")
+          .forEach(function(c) {
+            document.cookie = c.replace(/^ +/, "")
+              .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+        this.router.navigateByUrl('/', { skipLocationChange: false }).then(() => {
+          this.router.navigate(['/login']);
+          window.location.reload();
         });
       },
       (error) => {
         console.error('There was an error!', error);
+        return error.message.split("//");
       }
     )
   }
 
-  registrateUser(formData: {
-    Username: string,
-    Password: string,
-    PasswordConfirmation: string,
-    EmailAddress: string,
-  }) {
+  getCurrentUser() : Observable<User> {
+    return this.Http.get<User>(getBaseUrl() + 'api/user/current_user');
+  }
+
+  registerUser(formData: {
+    Username: string | null | undefined;
+    PasswordConfirmation: string | null | undefined;
+    EmailAddress: string | null | undefined;
+    Password: string | null | undefined
+  }) :string {
     this.Http.post<RegisterModel>(getBaseUrl() + 'api/user/registration', formData).subscribe(
       (data) => {
         this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
@@ -58,8 +68,10 @@ export class UserService {
       },
       (error) => {
         console.error('There was an error!', error);
+        return error.message.split("//");
       }
     )
+    return "";
   }
 
 }
