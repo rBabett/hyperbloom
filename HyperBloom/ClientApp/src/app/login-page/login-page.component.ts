20f5053d51c1d5bd39068b02ff2a864c5f9d8850
rBabett <richterbabett03@gmail.com>
2023-11-16 16:30:16 +0100

    add loginUser() and registerUser() methods

--- HyperBloom/ClientApp/src/app/login-page/login-page.component.ts
+++ HyperBloom/ClientApp/src/app/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
@@ -8,10 +12,16 @@ import {Component, OnInit} from '@angular/core';
 export class LoginPageComponent implements OnInit{
   backgroundImageUrl: string;
 
+  constructor(http: HttpClient,
+              @Inject('BASE_URL') baseUrl: string,
+              private Router: Router,
+              private formBuilder: FormBuilder,
+              public userService: UserService) {
+  }
+
   ngOnInit() {
     this.setRandomBackground();
   }
-
   setRandomBackground() {
     const numberOfImages = 20;
     const randomIndex = Math.floor(Math.random() * numberOfImages) + 1;
@@ -19,4 +29,32 @@ export class LoginPageComponent implements OnInit{
     this.backgroundImageUrl = `url('/assets/${randomIndex}.png')`;
   }
 
+  newUserForm = this.formBuilder.group({
+    username: '',
+    password: '',
+    passwordConfirmation: '',
+    emailAddress: ''
+  });
+
+  loginUser() {
+    const formData = {
+      Username: this.newUserForm.get('username')?.value,
+      Password: this.newUserForm.get('password')?.value,
+    }
+
+    let messages = this.userService.loginUser(formData);
+    console.log(messages);
+  }
+
+  registerUser() {
+    const formData = {
+      Username: this.newUserForm.get('username')?.value,
+      Password: this.newUserForm.get('password')?.value,
+      PasswordConfirmation: this.newUserForm.get('passwordConfirmation')?.value,
+      EmailAddress: this.newUserForm.get('emailAddress')?.value,
+    }
+
+    let messages = this.userService.registerUser(formData);
+    console.log(messages);
+  }
 }
