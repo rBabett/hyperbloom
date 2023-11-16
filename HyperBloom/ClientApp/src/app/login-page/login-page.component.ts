e2952f54df56e51b2114d9a652f12e9a3ca67ae9
rBabett <richterbabett03@gmail.com>
2023-11-16 13:38:53 +0100

    add login page with random background image

---
+++ HyperBloom/ClientApp/src/app/login-page/login-page.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit{
+  backgroundImageUrl: string;
+
+  ngOnInit() {
+    this.setRandomBackground();
+  }
+
+  setRandomBackground() {
+    const numberOfImages = 22;
+    const randomIndex = Math.floor(Math.random() * numberOfImages) + 1;
+
+    this.backgroundImageUrl = `url('/assets/${randomIndex}.png')`;
+  }
+
+}
