ebe65888ffb9ab56d2c5a2bbf2e7e78413425bfe
rBabett <richterbabett03@gmail.com>
2023-11-16 16:31:46 +0100

    add logout() method and logged in user check to navmenu

--- HyperBloom/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ HyperBloom/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  isLoggedIn = false;
+
+  constructor(private userService: UserService,
+              private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.checkIfUserIsLoggedIn();
+  }
+
+  private checkIfUserIsLoggedIn() {
+    this.userService.getCurrentUser().subscribe(
+      (currentUser) => {
+        console.log(currentUser);
+        if (currentUser != null) {
+          this.isLoggedIn = true;
+        }
+      },
+      (error) => {
+        this.isLoggedIn = false;
+      }
+    );
+  }
+
+  logout() {
+    this.userService.logoutUser();
+  }
 
   collapse() {
     this.isExpanded = false;
